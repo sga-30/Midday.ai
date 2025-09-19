199d07d1e8b7cea3b3d70bc47624ee7c27a8922d
Okoli Chidi <okcmaybac2236@gmail.com>
2025-09-19 14:56:43 +0100

    utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
-// lib/supabaseClient.ts
-import { createClient } from "@supabase/supabase-js";
+// lib/utils.ts
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export function cn(...inputs: (string | undefined | null | false)[]) {
+  return twMerge(clsx(inputs))
+}
